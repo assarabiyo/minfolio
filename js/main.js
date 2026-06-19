e1eb1e4a483cd12aeee9cf21bd9c4eda76784d1e
assarabiyo <ddurise@gmail.com>
2026-06-19 13:21:33 +0900

    work 모달 90% 완료, 디자인 조금씩 수정중

--- js/main.js
+++ js/main.js
@@ -12,28 +12,84 @@ document.querySelectorAll(".gnb a").forEach((link) => {
     });
 });
 
+// --------------------- 스와이퍼
+
 var swiper = new Swiper(".mainwork-list", {
     slidesPerView: 3,
     spaceBetween: 20,
-    effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    coverflowEffect: {
-        rotate: -15,
-        stretch: 0,
-        scale: 0.8,
-        depth: 30,
-        modifier: 1,
-        slideShadows: false,
-    },
-    fadeEffect: {
-        crossFade: true,
-    },
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
 });
+
+//  ----------------- 워크 모달
+
+const modal2 = document.querySelector(".work-modal-bg");
+const detail = document.querySelector(".modal-detail");
+const summary = detail.querySelector("summary");
+const linkBox = document.getElementById("link-box");
+const link = document.getElementById("w-link");
+
+const projects = {
+    m1: {
+        title: "서울 서버 '매우 혼잡' 해결하는 법.shorts",
+        video: "https://www.youtube.com/embed/5QuF0EI_Amc",
+        info: "2026.04.20~05.13\nAfter Effects · Illustrator",
+        purpose: "...",
+        process: "스토리보드 제작 → 일러스트 작업 → 애프터이펙트 모션 작업",
+        link: "",
+    },
+    m2: {
+        title: "기다려봐 체크체크",
+        video: "https://www.youtube.com/embed/5QuF0EI_Amc",
+        info: "2025.06\nㄷㄷㄷㄷㄷㄷ",
+        purpose: "...",
+        process: "스토리보드 제작 → 일러스트 작업 → 애프터이펙트 모션 작업",
+        link: "",
+    },
+};
+
+document.querySelectorAll(".work-card").forEach((card) => {
+    card.addEventListener("click", () => {
+        const id = card.dataset.project;
+
+        document.getElementById("modal-title").textContent = projects[id].title;
+        document.getElementById("modal-video").src = projects[id].video;
+        document.getElementById("w-info").innerText = projects[id].info;
+        document.getElementById("w-purpose").textContent = projects[id].purpose;
+        document.getElementById("w-process").textContent = projects[id].process;
+        document.getElementById("w-link").href = projects[id].link;
+
+        if (projects[id].link) {
+            linkBox.style.display = "block";
+
+            link.href = projects[id].link;
+        } else {
+            linkBox.style.display = "none";
+        }
+        detail.open = false;
+        summary.textContent = "▶ 더보기";
+        modal2.classList.add("active");
+    });
+});
+document.querySelector(".wmodal-close").addEventListener("click", () => {
+    modal2.classList.remove("active");
+});
+
+modal2.addEventListener("click", (e) => {
+    if (e.target === modal2) {
+        modal2.classList.remove("active");
+    }
+});
+
+detail.addEventListener("toggle", () => {
+    summary.textContent = detail.open ? "▼ 접기" : "▶ 더보기";
+});
+
+// ------------------------- 컨택 폼
 
 const form = document.getElementById("contactForm");
 const modal = document.querySelector(".success-modal");
@@ -80,13 +136,10 @@ modalClose.addEventListener("click", () => {
     modal.classList.remove("active");
 });
 
+document.querySelectorAll(".work-card").forEach((card) => {
+    card.addEventListener("click", () => {
+        console.log("클릭됨!");
 
-const modal2 = document.querySelector(".work-modal-bg");
-
-document.querySelector(".work-card").addEventListener("click",()=>{
-    modal2.classList.add("active");
-});
-
-document.querySelector(".wmodal-close").addEventListener("click",()=>{
-    modal2.classList.remove("active");
+        modal2.classList.add("active");
+    });
 });
